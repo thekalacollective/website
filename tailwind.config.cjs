7563d33d3ea04ba3f98a3326ef50b27cf1b6ca9e
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2022-09-01 06:30:45 +0530

    chore: update deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 };
