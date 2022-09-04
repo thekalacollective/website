93d85324699adca3a1f55fabdfbd941dcb9c4f79
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2022-09-04 11:31:05 +0530

    api endpoint for getting presigned urls

---
+++ src/pages/api/media.ts
@@ -0,0 +1,52 @@
+// src/pages/api/examples.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+import { env } from "../../env/server.mjs";
+
+type MediaUploadType = {
+  extension: string;
+  fileName: string;
+  userId: string;
+};
+
+import {
+  S3Client,
+  ListBucketsCommand,
+  ListObjectsV2Command,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const S3 = new S3Client({
+  region: "us-west-004",
+  endpoint: `https://s3.us-west-004.backblazeb2.com`,
+  credentials: {
+    accessKeyId: env.B2_ACCESS_KEY_ID,
+    secretAccessKey: env.B2_APPLICATION_KEY,
+  },
+});
+
+const restricted = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { fileName, extension, userId } = req.query as MediaUploadType;
+
+  let signedUrl = await getSignedUrl(
+    S3,
+    new PutObjectCommand({
+      Bucket: env.B2_BUCKET_NAME,
+      Key: `${userId}/${fileName}.${extension}`,
+      ContentType: `image/${extension}`,
+    }),
+    { expiresIn: 3600 }
+  ).catch((e) =>
+    res.status(500).send({ error: e, message: "An error occured" })
+  );
+
+  if (signedUrl) {
+    res.status(200).send(signedUrl);
+  } else {
+    res.status(500).send("An error occured");
+  }
+};
+
+export default restricted;
