8d4f3e74034ab6df6c93a797e7f2c5586a15ab87
Tejas Ahluwalia <tejasahluwalia@gmail.com>
2022-09-09 20:20:52 +0530

    seed function for location

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+import cities from "../cities";
+import normalize from "../src/utils/normalize";
+
+const states: string[] = cities.reduce((acc, city) => {
+  if (!acc.includes(normalize(city.admin_name))) {
+    acc.push(normalize(city.admin_name));
+  }
+  return acc;
+}, [] as string[]);
 
 const tags = [
   "Photojournalism",
@@ -46,6 +55,18 @@ const Survey = {
         label: "Training",
         key: "training",
       },
+      {
+        label: "Meeting like-minded people",
+        key: "meeting",
+      },
+      {
+        label: "Assignments/Jobs",
+        key: "assignments",
+      },
+      {
+        label: "Second shooter opportunities",
+        key: "second-shooter",
+      },
     ],
   },
   assistance: {
@@ -60,6 +81,84 @@ const Survey = {
       {
         label: "Preparing Rate Cards",
         key: "rateCards",
+      },
+      {
+        label: "Creating a portfolio",
+        key: "portfolio",
+      },
+      {
+        label: "Business Coaching",
+        key: "business",
+      },
+    ],
+  },
+  workshops: {
+    type: "checkbox",
+    label: "Which workshops would you like to attend?",
+    key: "workshops",
+    options: [
+      {
+        label: "Image post-processing",
+        key: "image-post-processing",
+      },
+      {
+        label: "Video post-processing",
+        key: "video-post-processing",
+      },
+      {
+        label: "Lighting",
+        key: "lighting",
+      },
+      {
+        label: "Album Making",
+        key: "album-making",
+      },
+    ],
+  },
+  mentorship: {
+    type: "textarea",
+    label:
+      "Here's your chance to give back! Are you open to share your knowledge and expertise with this community? If yes, please specify how you can help.",
+    hint: "For e.g. you can mentor an upcoming photographer / Videographer / teach technical or business skills / conduct workshops.",
+    key: "mentorship",
+  },
+  message: {
+    type: "textarea",
+    label: "Leave us a message!",
+    key: "message",
+  },
+  contact: {
+    type: "checkbox",
+    label: "How would you like to stay in touch?",
+    key: "contact",
+    options: [
+      {
+        label: "Facebook group",
+        key: "facebook",
+      },
+      {
+        label: "WhatsApp group",
+        key: "whatsapp",
+      },
+      {
+        label: "Discord",
+        key: "discord",
+      },
+      {
+        label: "Slack",
+        key: "slack",
+      },
+      {
+        label: "Google group",
+        key: "google",
+      },
+      {
+        label: "Newsletter",
+        key: "newsletter",
+      },
+      {
+        label: "Website/Forum",
+        key: "website",
       },
     ],
   },
@@ -112,6 +211,28 @@ async function main() {
       title: "Membership Application",
       schema: Survey,
     },
+  });
+
+  states.forEach(async (state) => {
+    await prisma.locationState.upsert({
+      where: {
+        name: state,
+      },
+      update: {},
+      create: {
+        name: state,
+        cities: {
+          createMany: {
+            data: [
+              ...cities
+                .filter((city) => normalize(city.admin_name) === state)
+                .map((city) => ({ name: normalize(city.city) })),
+              { name: "other" },
+            ],
+          },
+        },
+      },
+    });
   });
 }
 
